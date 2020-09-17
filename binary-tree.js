6422cf70cde9b80fae1906262954fa0d935a655b
bankymono <bankymono@gmail.com>
2020-09-17 22:07:52 +0100

    implemented lookup

--- binary-tree.js
+++ binary-tree.js
@@ -36,6 +36,58 @@ class BinarySearchTree{
             }
         }
     }
+
+    lookup(value){
+        if(!this.root){
+            return false;
+        }
+
+        let currentNode = this.root
+
+        while(currentNode){
+            if(value < currentNode.value){
+                currentNode = currentNode.left
+            } else if(value > currentNode.value){
+                currentNode = currentNode.right
+            }
+            else if(currentNode.value === value){
+                return currentNode;
+            }
+        }
+        return false
+    }
+
+    remove(value){
+        if(!this.root){
+            return false
+        }
+        let currentNode = this.root;
+        let parentNode = null;
+
+        while(currentNode){
+            if(value < currentNode.value){
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            }else if(value > currentNode.value){
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            }else if(currentNode.value === value){
+                if(currentNode.right === null){
+                    if(parentNode === null){
+                        this.root = currentNode.left
+                    }else{
+                        if(currentNode.value < parentNode.value){
+                            parentNode.left = currentNode.left;
+                        }else if(currentNode.value > parentNode.value){
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+                }else if(currentNode.left === null){
+
+                }
+            }
+        }
+    }
 }
 
 
@@ -47,8 +99,9 @@ tree.insert(20);
 tree.insert(170);
 tree.insert(15);
 tree.insert(1);
+console.log(tree.lookup(9))
 
-console.log(JSON.stringify(traverse(tree.root)))
+// console.log(JSON.stringify(traverse(tree.root)))
 
 function traverse(node){
     const tree = { value: node.value };
